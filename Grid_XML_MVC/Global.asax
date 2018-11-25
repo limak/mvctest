eb62465cca3fec25122f36a04bffbf17f93a8883
limak <ksitnik@gmail.com>
2018-11-25 14:39:52 -0500

    boop

---
+++ Grid_XML_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Grid_XML_MVC.MvcApplication" Language="C#" %>
---
+++ Grid_XML_MVC/Models/CustomerModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Grid_XML_MVC.Models
+{
+    public class CustomerModel
+    {
+        ///<summary>
+        /// Gets or sets CustomerId.
+        ///</summary>
+        public int CustomerId { get; set; }
+
+        ///<summary>
+        /// Gets or sets Name.
+        ///</summary>
+        public string Name { get; set; }
+
+        ///<summary>
+        /// Gets or sets Country.
+        ///</summary>
+        public string Country { get; set; }
+    }
+}
